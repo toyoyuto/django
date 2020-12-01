f9ce24054d0d2ac7eb2ab14371f9021529bb61e5
豊岡祐人 <toyooka.yuto@baroque-w.jp>
2020-12-01 11:38:25 +0900

    #デフォルト画面

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/accounts/login/')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
